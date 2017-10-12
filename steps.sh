ae36d0e9b5efae00fcb2066c022edcd9e9e8fa13
JiyanBlack <rglsm7655558@gmail.com>
2017-10-12 20:27:31 +0800

    add scripts

--- steps.sh
+++ steps.sh
@@ -1,7 +1,28 @@
-sudo apt-get update
+apt_update 'Update the apt cache daily' do
+  frequency 86400
+  action :periodic
+end
 
-sudo ufw allow 'Nginx Full'
-sudo apt-get install nginx
+package 'apache2'
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash
-sudo apt-get install -y nodejs
+service 'apache2' do
+  supports status: true
+  action [:enable, :start]
+end
+
+git '/home/ubuntu/website' do
+  repository 'https://github.com/JiyanBlack/cloudcomputing-group6.git'
+  revision 'master'
+  action :sync
+  user 'ubuntu'
+end
+
+execute "create apache static file director" do
+    command "sudo mkdir /var/www/html/"
+    user "ubuntu"
+end
+
+execute "copy files to apache director" do
+    command "sudo cp -R /home/ubuntu/website/server/static/* /var/www/html/"
+    user "ubuntu"
+end
